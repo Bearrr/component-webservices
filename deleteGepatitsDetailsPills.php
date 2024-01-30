8ca1f5f3e2dab8ccb9bcabb1b30c43ae4430e176
Bearrr <r.vany88@gmail.com>
2024-01-30 14:57:41 +0200

    Create deleteGepatitsDetailsPills.php

---
+++ deleteGepatitsDetailsPills.php
@@ -0,0 +1,27 @@
+<?php 
+     public function deleteGepatitsDetailsPills()
+    {
+
+        $db = FabrikWorker::getDbo();
+        $app = JFactory::getApplication();
+        $request = file_get_contents('php://input');
+        $data = json_decode($request, true);
+        $user = JFactory::getUser();
+        $userid = $user->id * 1;
+        $input = $app->input;
+        $dev = (int)$input->get('dev', 0);
+
+        if ($dev == 1) $userid = 509;
+        if ($userid == 0)
+            return json_encode(['success' => false, 'error' => 'Для запиту інформації необхідно пройти авторизацію. Спробуйте оновити сторінку та авторизуватись.']);
+
+
+        $object = new stdClass();
+        $object->id = (int)$data['id'];
+
+        $q = "delete from med_met_pill where id = $object->id";
+        $db->setQuery($q);
+        $db->execute();
+
+        return json_encode(['success' => true]);
+    }
